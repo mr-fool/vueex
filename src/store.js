c23ffe657f6a8fcd1012098cd42a47ee17c2d52c
mr-fool <mr-fool@users.noreply.github.com>
2020-04-25 00:06:47 -0600

    state

--- src/store.js
+++ src/store.js
@@ -4,7 +4,18 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'John Smith' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ]
+  },
   mutations: {},
   actions: {}
 })
