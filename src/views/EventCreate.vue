1e6e5f79149793c8481eb19c27e211cf1dda55d8
mr-fool <mr-fool@users.noreply.github.com>
2020-04-26 09:52:41 -0600

    refactoring

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <h1>Create an Event, {{ user.name }}</h1>
-    <p>This event was created by {{ user.name }}</p>
+    <p>This event was created by {{ user.id }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
   </div>
 </template>
 
