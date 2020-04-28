3060a2a298533ffbb208587daaeb5ae2be996004
mr-fool <mr-fool@users.noreply.github.com>
2020-04-27 22:17:53 -0600

    getter

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Create an Event, {{ user.name }}</h1>
     <p>This event was created by {{ user.id }}</p>
+    <p>There are {{ catLength }} categories</p>
     <ul>
       <li v-for="cat in categories" :key="cat">{{ cat }}</li>
     </ul>
@@ -11,10 +12,13 @@
 <script>
 import { mapState } from 'vuex'
 export default {
-  computed: mapState({
-    user: 'user',
-    categories: 'categories'
-  })
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength
+    },
+    ...mapState(['user'],['categories'])
+  }
+  
 }
 </script>
 
