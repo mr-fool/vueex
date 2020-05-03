8773f7883077e4003633ff31862ba8081878a873
mr-fool <mr-fool@users.noreply.github.com>
2020-05-03 15:40:46 -0600

    added full path

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,6 +2,13 @@
   <div>
     <h1>Events Listing</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event"/>
+    <template v-if="page != 1">
+      <router-link :to="{ name: 'event-list', query: { page: page - 1 } }" rel="prev">
+      Prev Page</router-link>
+      <template v-if="hasNextPage"> | </template>
+    </template>
+    <router-link v-if="hasNextPage" :to="{ name: 'event-list', query: { page: page + 1 } }" rel="next">
+      Next Page</router-link>
   </div>
 </template>
 
@@ -14,8 +21,22 @@ export default {
     EventCard
   },
   created() {
-    this.$store.dispatch('fetchEvents')
+    this.perPage = 3 // Setting perPage here and not in data means it won't be reactive.
+    // We don't need it to be reactive, and this way our component has access to it.
+
+    this.$store.dispatch('fetchEvents', {
+      perPage: this.perPage,
+      page: this.page
+    })
   },
-  computed: mapState(['events'])
+  computed: {
+    page() {
+      return parseInt(this.$route.query.page) || 1
+    },
+    hasNextPage() {
+      return this.eventsTotal > this.page * this.perPage
+    },
+    ...mapState(['events', 'eventsTotal'])
+  }
 }
 </script>
